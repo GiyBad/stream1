72a693cafbe66c8892fe33beb8fa454c61bfb5a7
GiyBad <limelauncher228@gmail.com>
2026-04-20 21:00:24 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -2,8 +2,7 @@ import subprocess
 import os
 import time
 
-# КОНФИГУРАЦИЯ
-INPUT_STREAM = "http://45.145.32.13:20440/match_futbol_2_hd/index.m3u8?token=test"
+INPUT_STREAM = "http://188.225.31.197/bpk-tv/000004499/tve/index.m3u8"
 LOGO_URL = "https://i.postimg.cc/nrtrd8t7/Gemini-Generated-Image-hf4jb6hf4jb6hf4j.jpg"
 
 def start_reencode():
@@ -14,33 +13,16 @@ def start_reencode():
         'ffmpeg',
         '-i', INPUT_STREAM,
         '-i', LOGO_URL,
-        '-filter_complex',
-        '[1:v]scale=120:-1[logo];'
-        '[0:v]fps=60,scale=1280:720[bg];'
-        '[bg][logo]overlay=main_w-overlay_w-20:20',
-        '-c:v', 'libx264',
-        '-preset', 'veryfast',
-        '-b:v', '3500k',
-        '-maxrate', '4000k',
-        '-bufsize', '6000k',
-        '-pix_fmt', 'yuv420p',
-        '-g', '60', # Кейфрейм каждую секунду для быстрой загрузки
-        '-c:a', 'aac',
-        '-b:a', '128k',
-        '-f', 'hls',
-        '-hls_time', '4',           # Длина одного фрагмента (4 сек)
-        '-hls_list_size', '10',      # Хранить только последние 10 фрагментов
-        '-hls_flags', 'delete_segments', # Удалять старые фрагменты
-        '-hls_segment_filename', 'hls/seg_%d.ts',
-        'hls/live.m3u8'              # Итоговый файл
+        '-filter_complex', '[1:v]scale=120:-1[logo];[0:v]fps=60,scale=1280:720[bg];[bg][logo]overlay=main_w-overlay_w-20:20',
+        '-c:v', 'libx264', '-preset', 'veryfast', '-b:v', '3500k',
+        '-c:a', 'aac', '-b:a', '128k',
+        '-f', 'hls', '-hls_time', '4', '-hls_list_size', '5', '-hls_flags', 'delete_segments',
+        'hls/live.m3u8'
     ]
 
-    print("Запуск перекодирования в M3U8...")
-    # Запускаем процесс в фоне или ограничиваем по времени для теста
     process = subprocess.Popen(ffmpeg_cmd)
-    
-    # Даем поработать 10 минут (в Actions можно увеличить)
-    time.sleep(600) 
+    print("Стримим 5 минут...")
+    time.sleep(300) # Поработает 5 минут и выключится, чтобы файлы загрузились
     process.terminate()
 
 if __name__ == "__main__":
